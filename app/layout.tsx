e2585242ec72ba6ba28a13344df87b208209e863
en-vang <enrique@si.team>
2024-02-06 09:57:24 -0500

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css'
+import { montserrat } from './ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <footer className='py-10 flex justify-center items-center'>
+        Copy right ABM ❤️
+      </footer>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ['latin']});
+export const lusitana = Lusitana({ weight: ['400', '700'],subsets: ['latin'] });
